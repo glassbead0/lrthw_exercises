f327719535638af407d0c37c9efa6f794c775421
Aaron Glasenapp <glassbead@gmail.com>
2014-06-27 12:03:11 -0600

    added comments to ex7.rb

--- Chapter_07/ex7.rb
+++ Chapter_07/ex7.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
 
+# these 4 lines print strings, using string formatting in line 5.
 puts 'Mary had a little lamb.'
 puts 'Its fleece was what as %s.' % 'snow'
 puts 'And everywhere that Mary went.'
 puts '.' * 10 # it displays the string 10 times
 
+# defining several variables as a string of one character.
 end1 = 'C'
 end2 = 'h'
 end3 = 'e'
@@ -18,7 +20,9 @@ end10 = 'g'
 end11 = 'e'
 end12 = 'r'
 
+# printing the concatenated variables defined above
 puts end1 + end2 + end3 + end4 + end5 + end6
 puts end7 + end8 + end9 + end10 + end11 + end12
 
+# this puts leaves a blank line in the terminal for a visual break.
 puts
