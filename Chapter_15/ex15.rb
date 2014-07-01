50879e32c0fff582f9ad2b4c909ea3ebd7d39bd7
Aaron Glasenapp <glassbead@gmail.com>
2014-07-01 11:24:59 -0600

    updated ex15.rb with comments

--- Chapter_15/ex15.rb
+++ Chapter_15/ex15.rb
@@ -5,14 +5,16 @@ filename = ARGV[0]
 
 # this is the prompt symbol to show when we want user input
 prompt = '-> '
-#
+# the FILE.open method retrieves a file external to the current file
 txt = File.open(filename)
 
 puts "Here's your file: #{filename}"
+# the read method will open file and return the content of that file
 puts txt.read
 
 puts "I'll also ask you to type it again:"
 print prompt
+# when ARGV is used, you need to use STDIN.gets instead of just gets
 file_again = STDIN.gets.chomp
 
 txt_again = File.open(file_again)
